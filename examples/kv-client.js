48770c8ccdb0760c706ef41db9752ef10b0b90f9
George Borisov <git@gir.me.uk>
2018-03-04 22:52:28 +0000

    add KV client example

---
+++ examples/kv-client.js
@@ -0,0 +1,21 @@
+/*jshint node:true, esversion:6*/
+'use strict';
+
+var etcd = require('etcd3-client');
+
+etcd.createClient('KV', { host: 'localhost', port: 2379 }, function (err, client) {
+    // put
+    client.put({ key: 'foo', value: 'foo', prev_kv: true }, function (err, res) {
+        console.log('PUT RESPONSE', res);
+
+        // range
+        client.range({ key: 'f', range_end: '' }, function (err, res) {
+            console.log('RANGE RESPONSE', res);
+
+            // deleteRange
+            client.deleteRange({ key: 'f', range_end: 'g', prev_kv: true }, function (err, res) {
+                console.log('DELETE-RANGE RESPONSE', res);
+            });
+        });
+    });
+});
